18b1fdc5843610cbee8d323bd765aa3ca09cb1e2
mohits1005 <mohit@chalkstreet.com>
2017-05-08 15:34:51 +0530

    Setting up firebase , adding libraries, adding test code that writes data in Firebase

--- app/src/main/java/com/example/mohits1005/assignment/MainActivity.java
+++ app/src/main/java/com/example/mohits1005/assignment/MainActivity.java
@@ -3,13 +3,65 @@ package com.example.mohits1005.assignment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.firebase.client.Firebase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Hello","world");
+        Firebase.setAndroidContext(this);
+        final String user = "moh";
+        final String pass = "12345";
+        String url = "https://myapplication-8e299.firebaseio.com/users.json";
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String s) {
+                Firebase reference = new Firebase("https://myapplication-8e299.firebaseio.com/users");
+
+                if(s.equals("null")) {
+                    reference.child(user).child("password").setValue(pass);
+                    Toast.makeText(MainActivity.this, "registration successful", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    try {
+                        JSONObject obj = new JSONObject(s);
+
+                        if (!obj.has(user)) {
+                            reference.child(user).child("password").setValue(pass);
+                            Toast.makeText(MainActivity.this, "registration successful", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "username already exists", Toast.LENGTH_LONG).show();
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                System.out.println("" + volleyError );
+            }
+        });
+
+        RequestQueue rQueue = Volley.newRequestQueue(MainActivity.this);
+        rQueue.add(request);
     }
 }
