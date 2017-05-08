34466bf8e7d74dddb1c5793bcb2d3f902c11bd3f
mohits1005 <mohit@chalkstreet.com>
2017-05-08 16:34:49 +0530

    Login module

--- app/src/main/java/com/example/mohits1005/assignment/MainActivity.java
+++ app/src/main/java/com/example/mohits1005/assignment/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.mohits1005.assignment;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -19,48 +23,72 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    TextView register;
+    EditText username, password;
+    Button loginButton;
+    String user, pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Hello","world");
-        Firebase.setAndroidContext(this);
-        final String user = "moh";
-        final String pass = "12345";
-        String url = "https://myapplication-8e299.firebaseio.com/users.json";
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+        register = (TextView)findViewById(R.id.register);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        loginButton = (Button)findViewById(R.id.loginButton);
+        /*Set onclick listener for login button*/
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(String s) {
-                if(s.equals("null")){
-                    Toast.makeText(MainActivity.this, "user not found", Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                user = username.getText().toString();
+                pass = password.getText().toString();
+
+                if(user.equals("")){
+                    username.setError("can't be blank");
+                }
+                else if(pass.equals("")){
+                    password.setError("can't be blank");
                 }
                 else{
-                    try {
-                        JSONObject obj = new JSONObject(s);
+                    /*Request data from Firebase*/
+                    Toast.makeText(MainActivity.this, "Loading", Toast.LENGTH_LONG).show();
+                    String url = "https://myapplication-8e299.firebaseio.com/users.json";
+                    StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+                        @Override
+                        public void onResponse(String s) {
+                            if(s.equals("null")){
+                                Toast.makeText(MainActivity.this, "user not found", Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                try {
+                                    JSONObject obj = new JSONObject(s);
 
-                        if(!obj.has(user)){
-                            Toast.makeText(MainActivity.this, "user not found", Toast.LENGTH_LONG).show();
+                                    if(!obj.has(user)){
+                                        Toast.makeText(MainActivity.this, "user not found", Toast.LENGTH_LONG).show();
+                                    }
+                                    else if(obj.getJSONObject(user).getString("password").equals(pass)){
+                                        /*If password matches*/
+                                        Toast.makeText(MainActivity.this, "Successfully logged in", Toast.LENGTH_LONG).show();
+                                    }
+                                    else {
+                                        Toast.makeText(MainActivity.this, "incorrect password", Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
                         }
-                        else if(obj.getJSONObject(user).getString("password").equals(pass)){
-                            Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_LONG).show();
+                    },new Response.ErrorListener(){
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            System.out.println("" + volleyError);
                         }
-                        else {
-                            Toast.makeText(MainActivity.this, "incorrect password", Toast.LENGTH_LONG).show();
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                    });
+
+                    RequestQueue rQueue = Volley.newRequestQueue(MainActivity.this);
+                    rQueue.add(request);
                 }
 
             }
-        },new Response.ErrorListener(){
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                System.out.println("" + volleyError);
-            }
         });
-
-        RequestQueue rQueue = Volley.newRequestQueue(MainActivity.this);
-        rQueue.add(request);
     }
 }
